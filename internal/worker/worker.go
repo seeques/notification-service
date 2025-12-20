7667d29b42a4512a8eca852f77bf8dbc8e0ce184
seeques <equestrianbuddy@gmail.com>
2025-12-20 18:25:10 +0300

    Add Start method for Work goroutines

--- internal/worker/worker.go
+++ internal/worker/worker.go
@@ -2,18 +2,44 @@ package worker
 
 import (
 	"context"
+	"sync"
 
-	"github.com/rs/zerolog/log"
 	"github.com/seeques/notification-service/internal/queue"
 )
 
-func Work(rc *queue.RedisClient) {
+type Pool struct {
+	workers int
+	queue *queue.Queue
+	processor Processor
+}
+
+func NewPool(wrs int, q *queue.Queue, proc Processor) *Pool {
+	return &Pool{
+		workers: wrs,
+		queue: q,
+		processor: proc,
+	}
+}
+
+func (p *Pool) Start(q *queue.Queue) {
+	var wg sync.WaitGroup
+
+	for i := 0; i < p.workers; i++ {
+		wg.Add(1)
+		defer wg.Done()
+		go Work(q)
+	}
+
+	wg.Wait()
+}
+
+func Work(q *queue.Queue) {
 	var lp LogProcessor
 	ctx := context.Background()
 	for {
-		retJob, err := rc.PopJob()
-		if err != nil {
-			log.Error().Err(err).Msg("Error processing job")
+		retJob, err := q.PopJob()
+		if err != nil || retJob == nil {
+			break
 		}
 
 		lp.Process(ctx, retJob)
