7024fd21945e6f511bf3482b4d848f248e9a63ad
seeques <equestrianbuddy@gmail.com>
2025-12-20 19:25:25 +0300

    Refactor work function and make it a method

--- internal/worker/pool.go
+++ internal/worker/pool.go
@@ -4,49 +4,52 @@ import (
 	"context"
 	"sync"
 
-	"github.com/seeques/notification-service/internal/queue"
 	"github.com/rs/zerolog/log"
+	"github.com/seeques/notification-service/internal/queue"
 )
 
 type Pool struct {
-	workers int
-	queue *queue.Queue
+	workers   int
+	queue     *queue.Queue
 	processor Processor
 }
 
 func NewPool(wrs int, q *queue.Queue, proc Processor) *Pool {
 	return &Pool{
-		workers: wrs,
-		queue: q,
+		workers:   wrs,
+		queue:     q,
 		processor: proc,
 	}
 }
 
-func (p *Pool) Start() {
+func (p *Pool) Start(ctx context.Context) {
 	var wg sync.WaitGroup
 
 	for i := 0; i < p.workers; i++ {
 		wg.Add(1)
 		go func(id int) {
 			defer wg.Done()
-			Work(p.queue, id)
+			p.work(ctx, id)
 		}(i)
 	}
 
 	wg.Wait()
 }
 
-func Work(q *queue.Queue, id int) {
-	var lp LogProcessor
-	ctx := context.Background()
+func (p *Pool) work(ctx context.Context, id int) {
 	for {
-		log.Info().Interface("Worker:", id).Msg("Picked up job")
-		retJob, err := q.PopJob()
-		if err != nil || retJob == nil {
-			break
-		}
+		select {
+		case <-ctx.Done():
+			return
+		default:
+			log.Info().Interface("Worker:", id).Msg("Picked up job")
+			retJob, err := p.queue.PopJob()
+			if err != nil || retJob == nil {
+				continue
+			}
 
-		lp.Process(ctx, retJob)
-		log.Info().Interface("Worker:", id).Msg("Finished the job")
+			p.processor.Process(ctx, retJob)
+			log.Info().Interface("Worker:", id).Msg("Finished the job")
+		}
 	}
 }
