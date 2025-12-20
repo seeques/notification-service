c569af75547cfc36dce6202595801d7ab7d6f887
seeques <equestrianbuddy@gmail.com>
2025-12-20 12:32:01 +0300

    Add config package

---
+++ internal/config/config.go
@@ -0,0 +1,19 @@
+package config
+
+import (
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	RedisURL string
+}
+
+func LoadConfig() Config {
+	godotenv.Load()
+
+	return Config{
+		RedisURL: os.Getenv("REDIS_URL")
+	}
+}
