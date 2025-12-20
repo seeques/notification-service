e763c07f1d5eb1fcebeb7f85dd874038e2f77f3a
seeques <equestrianbuddy@gmail.com>
2025-12-20 17:17:22 +0300

    Push 3 jobs and pop them with non-graceful return

--- main.go
+++ main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
-	"context"
+	"time"
 
 	"github.com/rs/zerolog/log"
 	"github.com/seeques/notification-service/internal/queue"
 	"github.com/seeques/notification-service/internal/config"
+	"github.com/seeques/notification-service/internal/worker"
 )
 
 func main() {
@@ -15,17 +16,22 @@ func main() {
 	if err != nil {
 		log.Fatal().Err(err).Msg("failed to create new redis client")
 	}
-
-	ctx := context.Background()
 	
-	err = rc.RedisClient.Set(ctx, "foo", "bar", 0).Err()
-	if err != nil {
-		log.Fatal().Err(err).Msg("failed to set a string")
+	job := queue.Job{
+		ID: "0",
+		Channel: "email@email.io",
+		Recipient: "recipient@email.io",
+		Subject: "test",
+		Body: "test body",
+		Attempts: 0,
+		CreatedAt: time.Now(),
 	}
 
-	val, err := rc.RedisClient.Get(ctx, "foo").Result()
-	if err != nil {
-		log.Fatal().Err(err).Msg("failed to get result")
+	for i := 0; i < 3; i++ {
+		rc.PushJob(&job)
 	}
-	log.Info().Str("foo:", val).Msg("")
+
+	log.Info().Interface("Job:", job).Msg("Job pushed")
+
+	worker.Work(rc)
 }
