b768e7846889abb86b07fe13f74c685c195cbcc6
seeques <equestrianbuddy@gmail.com>
2025-12-21 18:13:38 +0300

    Add test for templates getter function

--- internal/storage/templates_test.go
+++ internal/storage/templates_test.go
@@ -28,3 +28,36 @@ func TestCreateTemp(t *testing.T) {
 		t.Error("expected timestamp to be set")
 	}
 }
+
+func TestGetTemp(t *testing.T) {
+	s := setupTestStorage(t)
+	ctx := context.Background()
+
+	template := &Template{
+		Name: "test",
+		Subject: "test_subject",
+		Body: "test_body",
+	}
+
+	err := s.CreateTemp(ctx, template)
+	if err != nil {
+		t.Fatalf("failed to create a template: %v", err)
+	}
+
+	retTemp, err := s.GetTemp(ctx, template.Name)
+	if err != nil {
+		t.Fatalf("failed to get the template: %v", err)
+	}
+
+	if retTemp.ID != template.ID {
+		t.Error("ID does not match")
+	}
+
+	if retTemp.Subject != template.Subject {
+		t.Error("Subject does not match")
+	} 
+
+	if retTemp.CreatedAt != template.CreatedAt {
+		t.Error("creation time does not match")
+	}
+}
