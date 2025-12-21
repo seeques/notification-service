0bb73f4c3f088296ebe1194c77ec070d48fac904
seeques <equestrianbuddy@gmail.com>
2025-12-21 11:53:33 +0300

    Add template, preference and log structs

---
+++ internal/storage/types.go
@@ -0,0 +1,34 @@
+package storage
+
+import (
+	"time"
+)
+
+type Template struct {
+	ID        int       `json:"id"`
+	Name      string    `json:"name"`
+	Subject   string    `json:"subject"`
+	Body      string    `"json:"body"`
+	CreatedAt time.Time `json:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt"`
+}
+
+type Preference struct {
+	ID        int       `json:"id"`
+	UserID    int       `json:"userID"`
+	Channel   string    `json:"channel"`
+	Enabled   bool      `json:"enabled"`
+	CreatedAt time.Time `json:"createdAt"`
+}
+
+type Log struct {
+	ID        int       `json:"id"`
+	JobID     int       `json:"jobID"`
+	Channel   string    `json:"channel"`
+	Recipient string    `json:"recipient"`
+	Status    string    `json:"status"`
+	Attempts  int       `json:"attempts"`
+	ErrMsg    string    `json:"errorMessage"`
+	CreatedAt time.Time `json:"createdAt"`
+	SentAt    time.Time `json:"sentAt"`
+}
